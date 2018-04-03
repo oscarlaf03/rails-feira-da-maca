62b4c05407f3d1268ad4da31245eb4d43c8a1910
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-03 13:46:52 -0300

    Singed users can post questions to products, questions summary available at Products#show as Path Products/:id via views/products/show.html.erb

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,40 @@
+class QuestionsController < ApplicationController
+  before_action :set_product, only: [:create, :update, :new, :edit]
+  before_action :set_user, only: [:create, :new]
+  def new
+    @question = Question.new
+    @product = Product.find(params[:product_id])
+  end
+
+  def create
+    @question = Question.new(check_questions_params)
+    @question.asker = @user
+    @question.product = @product
+    if @question.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def check_questions_params
+    params.require(:question).permit(:question_description)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_product
+    @product = Product.find(params[:product_id])
+  end
+
+end
