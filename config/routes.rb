940a498b09a2bd385760643e6ddacbfd37744639
andre miotto <miotto082@gmail.com>
2018-04-03 14:05:45 -0300

    Product controller on the run

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/show'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/destroy'
+
+  get 'products/update'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
