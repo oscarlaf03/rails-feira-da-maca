01d2803260d9840ff5841f24c6b441dd26adc948
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-05 19:20:45 -0300

    photos fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     mount_uploader :product_photo, PhotoUploader
+    mount_uploader :category_photo, PhotoUploader
 
   belongs_to :owner, class_name: 'User', foreign_key: 'owner_id'
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405185143) do
+ActiveRecord::Schema.define(version: 20180405220127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180405185143) do
     t.datetime "updated_at", null: false
     t.string "product_photo"
     t.boolean "sold", default: false
+    t.string "category_photo"
   end
 
   create_table "questions", force: :cascade do |t|
