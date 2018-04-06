f2ad3407028dbec28d8b463241ef7baabb54c298
oscar ortiz <ortizg.oscar@gmail.com>
2018-04-06 16:27:00 -0300

    This time Ajax and validation does works

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,12 +6,5 @@ class Product < ApplicationRecord
   belongs_to :buyer, class_name: 'User', foreign_key: 'buyer_id', optional: true
   has_many :questions, dependent: :destroy
   has_many :askers, through: :questions
-
-
-  validates :product_type, :category, :description, :price, :memory, :color, presence: true, allow_nil: true
-
-
-
-
-
+  validates :product_type, :category, :description, :price, :memory, :color, presence: true
 end
